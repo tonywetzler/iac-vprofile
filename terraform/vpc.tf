6c9c0754a74e61920d9eb63283a30c13c010ecf3
devops4sure <devops4sure@gmail.com>
2024-11-06 13:10:49 -0500

    errors

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -19,7 +19,6 @@ module "vpc" {
     "kubernetes.io/role/elb"                      = 1
   }
   
-
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = 1
